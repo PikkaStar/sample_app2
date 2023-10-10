12fc989315afaf0ad2e702710cc6ce627aed4c6e
PikkaStar <yokotaka1226@gmail.com>
2023-10-10 02:17:35 +0000

    study finish

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,9 +4,14 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to '/homes/top'
+    @list = List.new(list_params)
+    if @list.save
+      flash[:notice] = "投稿に成功しました"
+    redirect_to list_path(@list.id)
+  else
+    flash.now[:alert] = "投稿に失敗しました"
+    render :new
+  end
   end
 
   def index
@@ -14,13 +19,26 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
   end
 
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to lists_path
+  end
   private
   def list_params
-    params.require(:list).permit(:title,:body)
+    params.require(:list).permit(:title,:body,:image)
   end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,15 @@
 class List < ApplicationRecord
+  validates :title,presence: true
+  validates :body,presence: true
+  validates :image,presence: true
+  has_one_attached :image
+
+  def get_image
+    if image.attached?
+      image
+    else
+      'no_image'
+    end
+  end
+
 end
